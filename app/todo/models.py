8bb69f2f411d1e0a2f51ccbb8452121e5260497a
geekwise-erica-salazar <57914301+geekwise-erica-salazar@users.noreply.github.com>
2020-01-01 18:40:48 -0800

    `Added models`

--- app/todo/models.py
+++ app/todo/models.py
@@ -7,3 +7,35 @@ class Todo(models.Model):
 
     def _str_(self):
         return self.title
+
+class Branches(models.Model):
+    branch_name = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.branch_name
+
+class Accounts(models.Model):
+    acc_name = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.acc_name
+
+class Customers(models.Model):
+    cus_name = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.cus_name
+
+class Products(models.Model):
+    pro_name = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.pro_name
--- app/todo/serializers.py
+++ app/todo/serializers.py
@@ -1,7 +1,33 @@
 from rest_framework import serializers
 from .models import Todo
+from .models import Branches
+from .models import Accounts
+from .models import Customers
+from .models import Products
+
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = ('id', 'title', 'description', 'completed')
+
+class BranchesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branches
+        fields = ('id','branch_name','description','completed')
+
+class AccountsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Accounts
+        fields = ('id','acc_name','description','completed')
+
+class CustomersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customers
+        fields = ('id','cus_name','description','completed')
+
+class ProductsSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Products
+        fields = ('id','pro_name','description','completed')
+
